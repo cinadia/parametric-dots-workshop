23a1fc88be2cd41a434e8d77fca178d7e7d9f0c7
cinadia <73308097+cinadia@users.noreply.github.com>
2022-08-26 18:22:11 -0700

    Create intermediate.py

---
+++ intermediate.py
@@ -0,0 +1,71 @@
+# V2 - INTERMEDIATE 
+# import statements
+
+import simplegui
+import time
+import math
+
+# set width and height of frame
+frame_width = 600
+frame_height = 600
+
+# fields for the dots
+dot_radius = 5
+dot_width = 1
+dot_colours = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple']
+
+dots = []
+    
+# returns [x, y] of parametric equation points
+def get_dot_coords(t):
+    
+    # circle
+    #return [(math.cos(t) * 100 + (0.5 * frame_width)), (math.sin(t) * 100 + (0.5 * frame_height))]
+        
+    # star
+    #return [((2 * math.cos(t) + 5 * math.cos(2*t/3)) * 10 + (0.5 * frame_width)), 
+    #        ((2 * math.sin(t) - 5 * math.sin(2*t/3)) * 10 + (0.5 * frame_height))]
+    
+    # flower
+    return [((math.cos(t) * math.sin(4*t)) * 100 + (0.5 * frame_width)), 
+            ((math.sin(t)*math.sin(4*t))*100 + (0.5 * frame_height))] 
+    
+
+# creates the next dot: (x, y)
+def new_dots(t):
+    coords = get_dot_coords(t)
+    #colour = get_dot_colour()
+    dots.append(coords)
+        
+def draw(canvas):
+    t = time.time() # get current time
+    new_dots(t)
+    
+    # draw each dot in 'dots' list onto canvas
+    for i in range(len(dots)):
+        canvas.draw_circle(dots[i],
+                           dot_radius, 
+                           dot_width, 
+                           dot_colours[i % len(dot_colours)], # cycle through dot colours in order
+                           dot_colours[i % len(dot_colours)])
+       
+    
+# create a frame 
+frame = simplegui.create_frame("Parametric Equations", frame_width, frame_height)
+
+# assign draw handler to frame
+frame.set_draw_handler(draw)
+
+# create time Handler
+def timer_handler():
+    draw
+    
+    
+# set and start the timer
+timer = simplegui.create_timer(500, timer_handler)
+timer.start()
+
+# start the frame animation
+frame.start()
+
+
